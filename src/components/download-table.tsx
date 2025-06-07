80d02ef9908aeb55d0847c51ffaf086a124abb01
Ronaldy <ronaldylf@gmail.com>
2025-06-07 17:24:26 -0300

    download table in .png feature

--- src/components/download-table.tsx
+++ src/components/download-table.tsx
@@ -1,9 +1,45 @@
 import { DownloadIcon } from 'lucide-react'
 import { Button } from './ui/button'
+import { RefObject } from 'react'
+import html2canvas from 'html2canvas-pro'
 
-export default function DownloadTable() {
+export default function DownloadTable({
+    printRef,
+    fileName,
+}: {
+    printRef: RefObject<HTMLDivElement>
+    fileName: string
+}) {
+    async function handleDownload() {
+        const element = printRef.current
+
+        if (!element) {
+            alert('Algo deu errado, tente movamente')
+            return
+        }
+
+        const canvas = await html2canvas(element, { logging: true })
+        const data = canvas.toDataURL('image/png')
+
+        // Downloads image
+        const link = document.createElement('a')
+        link.href = data
+        link.download = `${fileName}.png`
+
+        document.body.appendChild(link)
+
+        link.click()
+
+        document.body.removeChild(link)
+    }
+
     return (
-        <Button variant={'outline'} size={'icon'} className="cursor-pointer">
+        <Button
+            variant={'outline'}
+            size={'icon'}
+            className="cursor-pointer"
+            onClick={handleDownload}
+        >
             <DownloadIcon />
         </Button>
     )
