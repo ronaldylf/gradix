5b6bceea1685c63710611d04c5a95af33a76dcf9
Ronaldy <ronaldylf@gmail.com>
2025-06-21 12:16:17 -0300

    added previous page button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { auth } from '@/authentication/auth'
 import EditEmail from '@/components/edit-email'
 import EditPassword from '@/components/edit-password'
 import EditUsername from '@/components/edit-username'
@@ -12,7 +11,9 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
+import { MoveLeftIcon } from 'lucide-react'
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export default function UserProfilePanel() {
     const session = useSession()
@@ -23,12 +24,23 @@ export default function UserProfilePanel() {
     return (
         <div className="flex justify-center mt-5">
             <Card className="w-full max-w-lg">
-                <CardHeader>
-                    <CardTitle>Painel de Usuário</CardTitle>
-                    <CardDescription>
-                        Visualize e gerencie suas informações pessoais.
-                    </CardDescription>
-                </CardHeader>
+                <div className="flex w-full align-middle ml-2">
+                    <Link href="/dashboard" className="h-max">
+                        <Button
+                            size="icon"
+                            variant="outline"
+                            className="cursor-pointer"
+                        >
+                            <MoveLeftIcon />
+                        </Button>
+                    </Link>
+                    <CardHeader className="w-full">
+                        <CardTitle>Painel de Usuário</CardTitle>
+                        <CardDescription>
+                            Visualize e gerencie suas informações pessoais.
+                        </CardDescription>
+                    </CardHeader>
+                </div>
                 <CardContent className="space-y-6">
                     {/* Seção Nome */}
                     <div className="flex items-center justify-between border-b pb-4">
