b9de2c4d36f528779396464c4f73b55d3eccc34e
Ronaldy <ronaldylf@gmail.com>
2025-07-18 14:56:43 -0300

    fixed image blocking in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,6 @@ export { auth as middleware } from '@/authentication/auth'
 
 export const config = {
     matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|login|public|icon.png|$|signup).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|login|signup|$|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
